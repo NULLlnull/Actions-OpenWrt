7f1a735e7b75c7e36189ba44120419c8198b77db
dream <954356394@qq.com>
2022-09-03 12:46:38 +0800

    PVE-Simple修复主题包软件冲突问题,更改了两个diy.sh的位置(rm -rf openwrt/package/lean/luci-theme-argon)修改到diy2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,3 +13,7 @@
 # Modify default IP
 sed -i 's/192.168.1.1/172.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.128.0/g' package/base-files/files/bin/config_generate
+
+#更换argon主题
+rm -rf openwrt/package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
