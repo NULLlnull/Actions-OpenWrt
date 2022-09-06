58c30622900d94fe51d41bdb3ab0e534b8101c79
NULLlnull <35243163+NULLlnull@users.noreply.github.com>
2022-09-06 19:42:33 +0800

    对file进行了固定，有一些初始化设置。默认密码为<>

--- afterUpdateFeeds.sh
+++ afterUpdateFeeds.sh
@@ -6,14 +6,14 @@
 # See /LICENSE for more information.
 #
 # https://github.com/P3TERX/Actions-OpenWrt
-# File name: diy-part2.sh
+# File name: afterUpdateFeeds.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
 # Modify default IP
 #以后直接改File
-sed -i 's/192.168.1.1/172.168.10.1/g' package/base-files/files/bin/config_generate
-sed -i 's/255.255.255.0/255.255.128.0/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 #更换argon主题
 rm -rf openwrt/package/lean/luci-theme-argon
@@ -21,3 +21,8 @@ git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-a
 #全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
+
+#使用File大法，在编译好的时候先下载一份最新的GFW列表和大陆IP进去
+#wget -O files/etc/ssrplus/china_ssr.txt https://ispip.clang.cn/all_cn.txt
+#如果下面这个有用，那么删除项目中的files/etc/ssrplus/china_ssr.txt
+wget -O files/etc/ssrplus/china_ssr https://ispip.clang.cn/all_cn.txt
---
+++ files/updateChinaIp.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+rm /etc/ssrplus/china_ssr.txt
+wget -O /etc/ssrplus/china_ssr.txt https://ispip.clang.cn/all_cn.txt
