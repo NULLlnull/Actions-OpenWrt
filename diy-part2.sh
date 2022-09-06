6b5f13eb8fb0b7df2d4734dcaa8d187eae6eaebf
NULLlnull <35243163+NULLlnull@users.noreply.github.com>
2022-09-06 15:50:27 +0800

    删除了无用的fq插件。精简了APP

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,12 +11,13 @@
 #
 
 # Modify default IP
+#以后直接改File
 sed -i 's/192.168.1.1/172.168.10.1/g' package/base-files/files/bin/config_generate
 sed -i 's/255.255.255.0/255.255.128.0/g' package/base-files/files/bin/config_generate
 
 #更换argon主题
 rm -rf openwrt/package/lean/luci-theme-argon
-git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon  #argon-主题
+git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon #argon-主题
 #全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
 #增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
 #增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
