279d64016672e2efea4db960fa31b428d64459f9
NULLlnull <35243163+NULLlnull@users.noreply.github.com>
2022-09-08 00:32:55 +0800

    删除了两个无效的软件源

--- afterInstallFeeds.sh
+++ afterInstallFeeds.sh
@@ -20,14 +20,6 @@ rm -rf openwrt/package/lean/luci-theme-argon
 rm -rf openwrt/package/feeds/kenzo/luci-theme-argon
 git clone https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon #argon-主题
 #git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git package/luci-theme-argon #argon-主题
-#全新的[argon-主题]登录界面,图片背景跟随Bing.com，每天自动切换
-#增加可自定义登录背景功能，请自行将文件上传到/www/luci-static/argon/background 目录下，支持jpg png gif格式图片，主题将会优先显示自定义背景，多个背景为随机显示，系统默认依然为从bing获取
-#增加了可以强制锁定暗色模式的功能，如果需要，请登录ssh 输入：touch /etc/dark 即可开启，关闭请输入：rm -rf /etc/dark，关闭后颜色模式为跟随系统
-
-#因为有冲突，所以先注释了
-rm -rf openwrt/package/lean/luci-theme-neobird
-rm -rf openwrt/package/feeds/kenzo/luci-theme-neobird
-git clone https://github.com/thinktip/luci-theme-neobird.git package/luci-theme-neobird
 
 git clone https://github.com/NULLlnull/luci-theme-rosy.git package/luci-theme-rosy
 
