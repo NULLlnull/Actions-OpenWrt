a6964fb477edcde62a65f55d023911e3983cc2e8
dream <954356394@qq.com>
2022-12-13 09:12:17 +0800

    删除很多配置文件
    Signed-off-by: Dreams <954356394@qq.com>

--- afterInstallFeeds.sh
+++ afterInstallFeeds.sh
@@ -12,8 +12,8 @@
 
 # Modify default IP
 #以后直接改File
-#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-#sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/172.30.10.1/g' package/base-files/files/bin/config_generate
+sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 #更换argon主题
 rm -rf openwrt/package/lean/luci-theme-argon
